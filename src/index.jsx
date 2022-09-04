603e5e96dbac7271d146344082efe0ed9b876b84
anshulv1401 <anshulv1401@gmail.com>
2022-09-04 12:52:05 +0530

    index file changes

--- src/index.jsx
+++ src/index.jsx
@@ -17,17 +17,9 @@ accountService.refreshToken().finally(startApp);
 
 function startApp() { 
     render(
-        <div  style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-            backgroundImage: `url("https://www.freecodecamp.org/news/content/images/size/w2000/2021/06/w-qjCHPZbeXCQ-unsplash.jpg")`
-          }}>
         <Router history={history}>
             <App />
-        </Router>
-        </div>,
+        </Router>,
         document.getElementById('app')
     );
 }
